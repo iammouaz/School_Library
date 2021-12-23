824413fc37224be0882c2de6c4ab7962db610554
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-23 15:36:46 +0200

    add classroom class

--- person.rb
+++ person.rb
@@ -1,10 +1,19 @@
 require_relative 'corrector'
+require './rental'
+
 
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
-  def initialize(age: 11, name: 'Unknown', parent_permission: true); end
+  def initialize(age: 11, name: 'Unknown', parent_permission: true)
+    @id = id
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
+  end
 
   def can_use_services?
     of_age? || @parent_permission
