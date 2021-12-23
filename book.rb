542d4f7a0ebb8e9bd746ffb033de3f57b4d43b55
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-23 15:36:37 +0200

    add Book class

---
+++ book.rb
@@ -0,0 +1,11 @@
+require './rental'
+
+class Book
+attr_accessor :title, :author, :rentals
+    def initialize(title, author)
+        @title = title
+        @author = author
+        @rentals = []
+
+    end
+end
