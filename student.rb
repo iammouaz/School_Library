04803d7a0a38a430ff811fd7d51afdc6d78e579a
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-24 18:00:55 +0200

    Pry

--- student.rb
+++ student.rb
@@ -1,4 +1,5 @@
 require_relative 'person'
+require  'pry'
 
 class Student < Person
   attr_reader :classroom
@@ -7,8 +8,14 @@ class Student < Person
     super(age: age, name: name, parent_permission: parent_permission)
     @classroom = classroom
   end
-
   def play_hooky
     "¯\(ツ)/¯"
   end
 end
+
+puts Student.new(age:12, name:'Moaz')
+puts Student.new(age:21, name:'Bob')
+puts Student.new(age:'45', name:'Mike')
+puts Student.new(age:53, name:'Ahmad')
+puts Student.new(age:23, name:'Michel')
+puts Student.new(age:43, name:'Zein')
