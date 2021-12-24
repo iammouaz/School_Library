0cd4ff27e821bdd233bdb2078224e8b0d568c8de
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-12-24 15:45:23 +0200

    Create App class

---
+++ main.rb
@@ -0,0 +1,23 @@
+require './student'
+require './teacher'
+require './classroom'
+require './book'
+require './rental'
+
+class App
+  def initialize
+    @people = []
+    @books = []
+    @rentals = []
+    @options = {
+      '1' => 'List all books',
+      '2' => 'List all people',
+      '3' => 'Create a person',
+      '4' => 'Create a book',
+      '5' => 'Create a rental',
+      '6' => 'List all rentals for a given person',
+      '7' => 'Exit'
+    }
+  end
+
+  
